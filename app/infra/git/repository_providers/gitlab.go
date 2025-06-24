0b9ca5566463b1b1fdbc555610d0278c8f7aa07b
Iain <iain@humblyarrogant.io>
2025-06-24 19:06:06 +0200

    Add gitlab support

---
+++ app/infra/git/repository_providers/gitlab.go
@@ -0,0 +1,60 @@
+package repository_providers
+
+import (
+	"boneclone/app/domain"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+type GitlabRepositoryProvider struct {
+	gitlab *gitlab.Client
+	org    string
+}
+
+func (g GitlabRepositoryProvider) GetRepositories() ([]domain.GitRepository, error) {
+
+	trueValue := true
+	opts := &gitlab.ListGroupProjectsOptions{
+		IncludeSubGroups: &trueValue,
+		ListOptions: gitlab.ListOptions{
+			PerPage: 100,
+			Page:    1,
+		},
+	}
+
+	var output []domain.GitRepository
+	var allProjects []*gitlab.Project
+	for {
+
+		projects, resp, err := g.gitlab.Groups.ListGroupProjects(g.org, opts)
+		if err != nil {
+			return output, err
+		}
+
+		allProjects = append(allProjects, projects...)
+
+		if resp.NextPage == 0 {
+			break // No more pages
+		}
+		opts.Page = resp.NextPage
+	}
+
+	for _, project := range allProjects {
+
+		output = append(output, domain.GitRepository{
+			Name: project.Name,
+			Url:  project.HTTPURLToRepo,
+		})
+	}
+
+	return output, nil
+}
+
+func NewGitlabRepositoryProvider(token string, org string) (domain.GitRepositoryProvider, error) {
+	gitlab, err := gitlab.NewClient(token)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &GitlabRepositoryProvider{gitlab: gitlab, org: org}, nil
+}
--- app/infra/git/repository_providers/main.go
+++ app/infra/git/repository_providers/main.go
@@ -9,6 +9,8 @@ import (
 func NewProvider(config domain.ProviderConfig) (domain.GitRepositoryProvider, error) {
 	if strings.ToLower(config.Provider) == "github" {
 		return NewGithubRepositoryProvider(config.Token, config.Org)
+	} else if strings.ToLower(config.Provider) == "gitlab" {
+		return NewGitlabRepositoryProvider(config.Token, config.Org)
 	}
 
 	return nil, fmt.Errorf("unknown provider: %s", config.Provider)
